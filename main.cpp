dc527634a6ddededa96404cef71e294bb153c341
StepanTheFlowey <stepanmasirov@gmail.com>
2022-07-10 23:18:00 +0500

    now supports largeer images

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <set>
+#include <vector>
 #include <iostream>
 #include <fstream>
-#include <filesystem>
+#include <WinSock2.h>
 #include <SFML/Graphics.hpp>
+
 namespace fs = std::filesystem;
 
 template <typename T> class ArgsMan {
@@ -15,7 +17,7 @@ template <typename T> class ArgsMan {
 public:
 
   ArgsMan(int argc, T** argv) {
-    for(int i = 0; i < argc; i++) {
+    for(int i = 0; i < argc; ++i) {
       string arg(argv[i]);
       if(arg.substr(0, 2) == "--") {
         args.insert(arg.substr(2));
@@ -32,11 +34,11 @@ public:
     provided = argc != 1;
   }
 
-  bool isKeyProvided(T key) {
+  bool isKeyProvided(const T key) const {
     return keys.find(key) != keys.end();
   }
 
-  bool isArgProvided(string arg) {
+  bool isArgProvided(const string& arg) const {
     return args.find(arg) != args.end();
   }
 
@@ -44,15 +46,16 @@ public:
     return provided;
   }
 
-  size_t getParamsCount() {
+  size_t getParamsCount() const {
     return params.size();
   }
 
-  string getParam(size_t index) {
+  string getParam(const size_t& index) const {
     if(index >= params.size()) return "";
     return params[index];
   }
 
+#ifdef DEBUG
   void debug() {
     std::cout << "Keys: ";
     for(auto& i : keys) {
@@ -70,16 +73,10 @@ public:
     }
     std::cout << std::endl;
   }
+#endif // DEBUG
 };
 
-enum class EncodingType {
-  Null = 0,
-  Auto = 1,
-  BrickImage,
-  ArduinoArray
-};
-
-void encodeImage(std::string in, std::string out, EncodingType type = EncodingType::BrickImage) {
+void encodeImage(const std::string& in, const std::string& out) {
   sf::Image image;
   if(!image.loadFromFile(in)) {
     std::cout << "Error file reading!" << std::endl;
@@ -87,59 +84,62 @@ void encodeImage(std::string in, std::string out, EncodingType type = EncodingTy
   }
 
   sf::Vector2u size = image.getSize();
-  if(size.x > 255 || size.y > 255) {
-    std::cout << "Input image is too big!" << std::endl;
-    std::cout << "Image size is: " << size.x << "x" << size.y << "." << std::endl;
-    std::cout << "Max size is: 255x255." << std::endl;
+  if(size.x > UINT16_MAX || size.y > UINT16_MAX) {
+    std::cout << "Input image is too big!\n";
+    std::cout << "Image size is: " << size.x << 'x' << size.y << '\n';
+    std::cout << "Max size is: " << UINT16_MAX << 'x' << UINT16_MAX << std::endl;
     exit(EXIT_FAILURE);
   }
   uint32_t dataSize = size.x * size.y * 4;
 
-  if(fs::exists(out)) {
-    fs::remove(out);
-  }
-
-  std::ofstream file(out);
+  std::ofstream file(out, std::ios_base::binary | std::ios_base::trunc);
 
   if(!file) {
     std::cout << "Error opening file for writing!" << std::endl;
     exit(EXIT_FAILURE);
   }
 
+  sf::Vector2<uint16_t> size16(
+    htons(static_cast<uint16_t>(size.x)),
+    htons(static_cast<uint16_t>(size.y))
+  );
+
   file << "flBIv1";
-  file.put(static_cast<uint8_t>(size.x));
-  file.put(static_cast<uint8_t>(size.y));
+  file.write(reinterpret_cast<char*>(&size16), sizeof(size16));
   file.write(reinterpret_cast<const char*>(image.getPixelsPtr()), dataSize);
   file.close();
-  std::cout << "Image succefully encoded to " << out << "." << std::endl;
 }
 
-void decodeImage(std::string in, std::string out) {
-  std::ifstream file(in);
+void decodeImage(const std::string& in, const std::string& out) {
+  std::ifstream file(in, std::ios_base::binary);
 
   if(!file) {
     std::cout << "Error opening file for reading!" << std::endl;
     exit(EXIT_FAILURE);
   }
 
-  char buff[7] {};
+  char buff[7]{};
   file.read(buff, 6);
   if(file.gcount() != 6) {
     std::cout << "File header reading error!" << std::endl;
     exit(EXIT_FAILURE);
   }
 
-  sf::Vector2<uint8_t> size;
-  file.read(reinterpret_cast<char*>(&size), 2);
+  sf::Vector2<uint16_t> size;
+  if(file.read(reinterpret_cast<char*>(&size), sizeof(size)).gcount() != sizeof(size)) {
+
+  }
+  size.x = ntohs(size.x);
+  size.y = ntohs(size.y);
 
   if(size.x == 0 || size.y == 0) {
-    std::cout << "File image size invalid!" << std::endl;
-    std::cout << "Readed size: " << size.x << "x" << size.y << std::endl;
+    std::cout << "File image size invalid!\n";
+    std::cout << "Readed size: " << size.x << 'x' << size.y << std::endl;
     exit(EXIT_FAILURE);
   }
 
   std::vector<sf::Color> data;
-  data.resize(static_cast<uint16_t>(size.x) * size.y);
+  data.resize(static_cast<size_t>(size.x) * size.y);
 
   file.read(reinterpret_cast<char*>(data.data()), static_cast<uint64_t>(size.x) * size.y * 4);
   if(file.gcount() != static_cast<uint64_t>(size.x) * size.y * 4) {
@@ -148,28 +148,25 @@ void decodeImage(std::string in, std::string out) {
   }
   file.close();
 
-  if(fs::exists(out)) {
-    fs::remove(out);
-  }
-
   sf::Image image;
   image.create(size.x, size.y, reinterpret_cast<sf::Uint8*>(data.data()));
   if(!image.saveToFile(out)) {
     std::cout << "Error saving output file!" << std::endl;
     exit(EXIT_FAILURE);
   }
-  std::cout << "Image succefully decoded to " << out << "." << std::endl;
 }
 
 int main(int argc, char** argv) {
+  std::ios::sync_with_stdio(false);
+
   ArgsMan<char> arguments(argc, argv);
 
 #ifdef DEBUG
   arguments.debug();
 #endif // DEBUG
 
-  if(!arguments.isProvided()) {
-    std::cout << std::endl << "No input file!" << std::endl;
+  if(!arguments.isProvided() || arguments.getParamsCount() == 0) {
+    std::cout << "No input file!" << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -178,7 +175,7 @@ int main(int argc, char** argv) {
       decodeImage(arguments.getParam(1), arguments.getParam(2));
     }
     else {
-      decodeImage(arguments.getParam(1), "out.bin");
+      decodeImage(arguments.getParam(1), "out.png");
     }
   }
   else {
